ad22da72d6157d65e8359b829425bd3b8cb12d58
isaquerchaves <isaque.ramosch@gmail.com>
2023-10-30 22:23:08 -0300

    feat: adicionando formulário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 export default function Home() {
   return (
-    <div>Criando pastas</div>
+    <div>
+      <h1>Biblioteca de Jogos</h1>
+      <form className="flex items-end gap-[2rem]">
+        <div>
+          <label htmlFor="title">Título:</label>
+          <input type="text" name="title" id="title" />
+        </div>
+        <div>
+          <label htmlFor="cover">Capa:</label>
+          <input type="text" name="cover" id="cover"/>
+        </div>
+        <div className="flex items-end p-4">
+        <button type="submit" className="bg-[rgba(26,26,26)] border-transparent text-base font-semibold cursor-pointer transition border-gray-400 hover:border-white"> 
+          Adicionar à biblioteca
+        </button>
+        </div>
+      </form>
+    </div>
   )
 }
